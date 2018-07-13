8364a1b5266baa2c17762f96cceccbbf4cb7083c
Goswami <vidit.goswami@metronom-external.com>
2018-07-13 17:52:08 +0530

    initial

---
+++ reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+// import alertReducer from './alert';
+// import weatherReducer from './weather';
+// import weatherListReducer from './weather_list';
+// import notificationReducer from './notification';
+
+export default combineReducers({
+    
+});
